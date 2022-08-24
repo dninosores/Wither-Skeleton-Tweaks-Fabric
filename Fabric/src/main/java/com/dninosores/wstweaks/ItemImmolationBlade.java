1069585a70438c48ead48a84612e207574647ef5
dninosores <dninosores@gmail.com>
2022-08-24 05:41:23 -0700

    Immolation blades make skeletons drop heads and cause lightning strikes

--- Fabric/src/main/java/com/dninosores/wstweaks/ItemImmolationBlade.java
+++ Fabric/src/main/java/com/dninosores/wstweaks/ItemImmolationBlade.java
@@ -1,6 +1,8 @@
 package com.dninosores.wstweaks;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LightningEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.AbstractSkeletonEntity;
@@ -23,6 +25,9 @@ public class ItemImmolationBlade extends SwordItem {
         if (target instanceof AbstractSkeletonEntity) {
             target.setHealth(1);
             target.damage(DamageSource.LIGHTNING_BOLT, 150);
+            LightningEntity lightning = new LightningEntity(EntityType.LIGHTNING_BOLT, target.getWorld());
+            lightning.setPosition(target.getPos());
+            target.getWorld().spawnEntity(lightning);
             double i = target.getWorld().random.nextDouble() * 4.0D;
             double d = target.getWorld().random.nextDouble() * 4.0D;
             double k = target.getWorld().random.nextDouble() * 4.0D;
--- Fabric/src/main/java/com/dninosores/wstweaks/WitherSkeletonTweaks.java
+++ Fabric/src/main/java/com/dninosores/wstweaks/WitherSkeletonTweaks.java
@@ -6,22 +6,34 @@ import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
 import net.fabricmc.fabric.api.loot.v2.FabricLootPoolBuilder;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.fabricmc.fabric.api.loot.v2.LootTableSource;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Material;
+import net.minecraft.block.SkullBlock;
+import net.minecraft.block.WitherSkullBlock;
 import net.minecraft.entity.mob.WitherSkeletonEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Items;
+import net.minecraft.item.SkullItem;
 import net.minecraft.loot.LootManager;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTable;
-import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.condition.*;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.LootNumberProvider;
+import net.minecraft.predicate.PlayerPredicate;
+import net.minecraft.predicate.entity.DamageSourcePredicate;
+import net.minecraft.predicate.entity.EntityEquipmentPredicate;
+import net.minecraft.predicate.entity.EntityPredicate;
+import net.minecraft.predicate.item.ItemPredicate;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.lwjgl.system.CallbackI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.spongepowered.asm.util.Locals;
 
 public class WitherSkeletonTweaks implements ModInitializer {
     // This logger is used to write text to the console and the log file.
@@ -36,9 +48,14 @@ public class WitherSkeletonTweaks implements ModInitializer {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(NAMESPACE);
     public static final Item FRAGMENT = new Item(new FabricItemSettings().group(ItemGroup.MISC));
-
+    public static final Item LAVA_BLADE = new ItemImmolationBlade();
+    public static final Item BLAZE_BLADE = new ItemImmolationBlade();
     public static final Identifier WITHER_SKELETON_TABLE =
             new Identifier("minecraft", "entities/wither_skeleton");
+    public static final Identifier SKELETON_TABLE =
+            new Identifier("minecraft", "entities/skeleton");
+    public static final Identifier STRAY_TABLE =
+            new Identifier("minecraft", "entities/stray");
 
     @Override
     public void onInitialize() {
@@ -49,16 +66,29 @@ public class WitherSkeletonTweaks implements ModInitializer {
         LOGGER.info("Initializing Wither Skeleton Tweaks (Fabric)");
 
         Registry.register(Registry.ITEM, new Identifier(NAMESPACE, FRAGMENT_ID), FRAGMENT);
-        Registry.register(Registry.ITEM, new Identifier(NAMESPACE, "lava_blade"), new ItemImmolationBlade());
-        Registry.register(Registry.ITEM, new Identifier(NAMESPACE, "blaze_blade"), new ItemImmolationBlade());
+        Registry.register(Registry.ITEM, new Identifier(NAMESPACE, "lava_blade"), LAVA_BLADE);
+        Registry.register(Registry.ITEM, new Identifier(NAMESPACE, "blaze_blade"), BLAZE_BLADE);
 
         LootTableEvents.MODIFY.register((ResourceManager resourceManager, LootManager lootManager, Identifier id, LootTable.Builder tableBuilder, LootTableSource source) -> {
+            LootPool.Builder skullDrop = LootPool.builder()
+                    .rolls(ConstantLootNumberProvider.create(1))
+                    .conditionally(new MatchToolLootCondition(ItemPredicate.Builder
+                            .create().items(LAVA_BLADE, BLAZE_BLADE).build()));
             if (WITHER_SKELETON_TABLE.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
                         .conditionally(RandomChanceLootCondition.builder(0.9f).build())
                         .with(ItemEntry.builder(FRAGMENT));
                 tableBuilder.pool(poolBuilder);
+
+                LootPool.Builder poolBuilder2 =
+                        skullDrop.with(ItemEntry.builder(Items.WITHER_SKELETON_SKULL));
+                tableBuilder.pool(poolBuilder2);
+            }
+            if (SKELETON_TABLE.equals(id) || STRAY_TABLE.equals(id)) {
+                LootPool.Builder poolBuilder2 =
+                        skullDrop.with(ItemEntry.builder(Items.SKELETON_SKULL));
+                tableBuilder.pool(poolBuilder2);
             }
         });
     }
